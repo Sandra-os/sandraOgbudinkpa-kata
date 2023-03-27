ee39d3c796d3f6bc029eb78473a03e2ea3b41bf1
Sandra-os <63932066+Sandra-os@users.noreply.github.com>
2023-03-27 10:26:34 +0000

    empty space in a single row

--- test_maze_solver.py
+++ test_maze_solver.py
@@ -1,3 +1,10 @@
 # Test cases 
-# Walls are represented with 1 and empty spaces with 0.
+# Walls are represented with 1 and empty spaces with 0 
 
+#Enpty space in a single row
+def empty_space():
+    maze = [1,1,1,1,0,1,1,1]
+
+#through hallway
+def hallway():
+    maze = 
