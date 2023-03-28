e1ed92a32d810e48433a60cfd51f9639d38f7503
Sandra-os <63932066+Sandra-os@users.noreply.github.com>
2023-03-28 14:23:36 +0000

    test cases were created for specific user stories

--- test_maze_solver.py
+++ test_maze_solver.py
@@ -1,10 +1,60 @@
 # Test cases 
-# Walls are represented with 1 and empty spaces with 0 
+# Walls are represented with 1 and empty spaces with 0 using n*m matrices. 2 represents a start position
+# and 3 an end position.
 
-#Enpty space in a single row
+# Empty space in a single row, user story 1
 def empty_space():
-    maze = [1,1,1,1,0,1,1,1]
+    maze = [1, 1, 1, 1, 0, 1, 1, 1]
 
-#through hallway
-def hallway():
-    maze = 
+# through hallway, user story 2
+def hallway():                  # Function to test hallway maze
+    maze = [
+        [1, 2, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1],
+        [1, 3, 1, 1, 1, 1]
+    ]
+
+# finding a way in and out of a room, user story 3
+def through_room():
+    maze = [
+        [1, 2, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1],
+        [1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 3, 1]
+    ]
+
+# test to follow winding path, user story 4
+def winding_path():
+    maze = [
+        [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1]
+    ]
+
+# test to reach the end of a maze, even if there are dead ends, user story 5
+def dead_end():
+    [
+        [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1]
+    ]
+
